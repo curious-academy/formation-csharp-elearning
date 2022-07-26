28b1e5b4a7904057d28d4d4049d19ed440bfacd2
Evan BOISSONNOT <evan@boissonnot.fr>
2022-07-26 11:36:13 +0200

    feat: fin quete 2

---
+++ Quetes/003/JeuVideo/HarryPotter.Games/HarryPotter.Games.UI.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Diagnostics;
+
+string titre = "Harry Potter game";
+
+Debug.WriteLine("==> Je teste" + titre);
+
+Console.WriteLine(titre);
+
