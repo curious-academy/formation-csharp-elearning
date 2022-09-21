8df14c1baf86bc256a0e56b8d8e99a12046240dc
Evan BOISSONNOT <evan@boissonnot.fr>
2022-09-21 17:35:16 +0200

    feat: debut quete 13

---
+++ Quetes/013/JeuVideo/HarryPotter.Games/HarryPotter.Games.Core/Character.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotter.Games.Core
+{
+    /// <summary>
+    /// Classe parente
+    /// </summary>
+    public class Character
+    {
+        #region Constructors
+        public Character()
+        {
+            this.Prenom = String.Empty;
+        }
+
+        public Character(string prenom)
+        {
+            this.Prenom= prenom;
+        }
+        #endregion
+
+        #region Public methods
+        /// <summary>
+        /// Méthode qui permet de changer la position du joueur
+        /// </summary>
+        public virtual void SeDeplacer()
+        {
+            System.Console.WriteLine($"{this.Prenom} Je me déplace");
+        }
+
+        /// <summary>
+        /// Méthode qui permet de changer la position du joueur
+        /// </summary>
+        public void SeDeplacer(Position newPosition)
+        {
+            this.SeDeplacer();
+            this.CurrentPosition = newPosition;
+        }
+
+        public void Attaquer(Character enemy)
+        {
+            System.Console.WriteLine("J'attaque l'ennemi {0}", enemy);
+        }
+        #endregion
+
+        #region Properties
+        public string Prenom { get; set; } = String.Empty;
+
+        public Position CurrentPosition { get; set; } = new Position() { X = 0, Y = 0 };
+        #endregion
+    }
+}
