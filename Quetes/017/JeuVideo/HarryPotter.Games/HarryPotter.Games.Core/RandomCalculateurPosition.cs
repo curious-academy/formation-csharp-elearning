e33a12d04434bc9cf7faa3c3ceb9c8851c55fff1
Evan BOISSONNOT <evan@boissonnot.fr>
2022-11-04 10:50:00 +0100

    fix: le projet ne s'exécutait pas => xmlinclude

---
+++ Quetes/017/JeuVideo/HarryPotter.Games/HarryPotter.Games.Core/RandomCalculateurPosition.cs
@@ -0,0 +1,26 @@
+﻿using HarryPotter.Games.Core.Interfaces;
+using HarryPotter.Games.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotter.Games.Core
+{
+    public class RandomCalculateurPosition : ICalculateurPosition
+    {
+        #region Public methods
+        public Position Calculer()
+        {
+            Random rand = new Random();
+
+            return new Position()
+            {
+                X = rand.Next(0, 100),
+                Y = rand.Next(0, 100)
+            };
+        }
+        #endregion
+    }
+}
