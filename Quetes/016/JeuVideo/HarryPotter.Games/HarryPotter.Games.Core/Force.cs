65c16aa1f889a3a1db3b1fa68684c92176b3d59e
Evan BOISSONNOT <evan@boissonnot.fr>
2022-11-04 10:31:46 +0100

    feat: fin quete 16

---
+++ Quetes/016/JeuVideo/HarryPotter.Games/HarryPotter.Games.Core/Force.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotter.Games.Core
+{
+    /// <summary>
+    /// Force du joueur/de la joueuse
+    /// </summary>
+    public abstract class Force
+    {
+        #region Constructors
+        public Force(string libelle)
+        {
+            this.Libelle = libelle;
+        }
+        #endregion
+
+        #region Public methods
+        public override string ToString()
+        {
+            return String.Format("{0}. {1}", this.Id, this.Libelle);
+        }
+        #endregion
+
+        #region Properties
+        public int Id { get; set; }
+
+        public string Libelle { get; set; } = string.Empty;
+        #endregion
+    }
+}
