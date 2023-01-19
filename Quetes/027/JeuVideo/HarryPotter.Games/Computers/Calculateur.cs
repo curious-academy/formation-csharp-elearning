644c8a109802dc3f3679b5306cc16eeb15d5d1aa
Evan BOISSONNOT <evan@boissonnot.fr>
2023-01-19 17:27:46 +0100

    feat: fin quete 27

---
+++ Quetes/027/JeuVideo/HarryPotter.Games/Computers/Calculateur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computers
+{
+    public class Calculateur
+    {
+        public float Calculer(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
